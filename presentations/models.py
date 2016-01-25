51599d425f8360f210306d617ff6f31381d19234
Avis <aao.t@yandex.ru>
2016-01-26 00:14:10 +0300

    Шаблоны ответов + модели Вопросов, Вариантов ответов и ОтветовПользователя

--- presentations/models.py
+++ presentations/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django.db.models import CharField, IntegerField, TextField
 
 
 class Organisation(models.Model):
@@ -36,3 +37,33 @@ class CoreSlide(models.Model):
 
     class Meta:
         db_table = 'slides'
+
+
+
+class Questions(models.Model):
+    presentation_number = IntegerField(verbose_name='Номер презентации') # Ссылка на номер обучающей презентации
+    question_number = IntegerField(verbose_name='Номер вопроса')
+    question_text = TextField(verbose_name='Текст вопроса')
+    answer_type = IntegerField(verbose_name='Тип ответа') # Номер типа вопроса (0 - да/нет; 1 - список; 2 - список с комментарием)
+    # TODO Создать модель Типы ответов
+    right_answer = IntegerField(verbose_name='Правильный ответ') # Номер правильного ответа
+
+    class Meta:
+        db_table = 'questions'
+
+class AnswerVariants(models.Model):
+    question = models.ForeignKey(Questions)
+    answer_number = IntegerField(verbose_name='Номер ответа') # Порядковый номер ответа
+    answer_text = CharField(verbose_name='Текст ответа', max_length=64)
+
+    class Meta:
+        db_table = 'answer_variants'
+
+
+class UserAnswers(models.Model):
+    answer_variants = models.ForeignKey(AnswerVariants)
+    user_id = IntegerField(verbose_name="Код пользователя")
+    answer_number = IntegerField(verbose_name='Номер ответа')
+    answer_comment = TextField(verbose_name='Комментарий') # используется только при Типе вопроса "Список с комментарием"
+    # может быть лучше вынести в отдельную модель
+
