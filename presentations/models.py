56063027d058a3176caaba3d74d046f9b43bcf22
wad <suguby@gmail.com>
2016-01-26 18:09:37 +0300

    подкорректировал модели

--- presentations/models.py
+++ presentations/models.py
@@ -3,6 +3,8 @@
 from django.db import models
 from django.db.models import CharField, IntegerField, TextField
 
+from user_interface.models import ProjectUser
+
 
 class Organisation(models.Model):
     name = models.CharField(verbose_name='Название организации', max_length=64)
@@ -39,31 +41,34 @@ class CoreSlide(models.Model):
         db_table = 'slides'
 
 
-
 class Questions(models.Model):
-    presentation_number = IntegerField(verbose_name='Номер презентации') # Ссылка на номер обучающей презентации
-    question_number = IntegerField(verbose_name='Номер вопроса')
-    question_text = TextField(verbose_name='Текст вопроса')
-    answer_type = IntegerField(verbose_name='Тип ответа') # Номер типа вопроса (0 - да/нет; 1 - список; 2 - список с комментарием)
-    # TODO Создать модель Типы ответов
-    right_answer = IntegerField(verbose_name='Правильный ответ') # Номер правильного ответа
+    presentation_number = models.ForeignKey(Presentation, verbose_name='Номер презентации')
+    number = IntegerField(verbose_name='Номер вопроса')
+    text = TextField(verbose_name='Текст вопроса')
+    answer_type = IntegerField(verbose_name='Тип ответа')
+    # TODO просто будем наследовать модели от этой
+    right_answer = models.BooleanField(verbose_name='Является правильным ответом')
 
     class Meta:
         db_table = 'questions'
 
-class AnswerVariants(models.Model):
+
+class Answers(models.Model):
     question = models.ForeignKey(Questions)
-    answer_number = IntegerField(verbose_name='Номер ответа') # Порядковый номер ответа
-    answer_text = CharField(verbose_name='Текст ответа', max_length=64)
+    text = CharField(verbose_name='Текст ответа', max_length=64)
 
     class Meta:
-        db_table = 'answer_variants'
+        db_table = 'answers'
 
 
 class UserAnswers(models.Model):
-    answer_variants = models.ForeignKey(AnswerVariants)
-    user_id = IntegerField(verbose_name="Код пользователя")
-    answer_number = IntegerField(verbose_name='Номер ответа')
-    answer_comment = TextField(verbose_name='Комментарий') # используется только при Типе вопроса "Список с комментарием"
-    # может быть лучше вынести в отдельную модель
+    """
+        наличие этой записи соответствует ответу user на вариант answer
+        иногда может быть комментарий к ответу
+    """
+    user = models.ForeignKey(ProjectUser, verbose_name="Пользователь")
+    answer = models.ForeignKey(Answers, verbose_name="Ответ")
+    comment = TextField(verbose_name='Комментарий', blank=True, null=True)
 
+    class Meta:
+        db_table = 'user_answers'
