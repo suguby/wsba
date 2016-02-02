5ecce36094c0053c71b9d22a0949283ac99b93d9
Artem Ulko <ulkoart@gmail.com>
2016-02-02 18:59:32 +0300

    подсчет презентаций

--- user_interface/admin.py
+++ user_interface/admin.py
@@ -2,7 +2,8 @@
 
 from django.contrib import admin
 from user_interface.models import *
-from presentations.models import Organisation
+from presentations.models import Organisation, Presentation
 
 admin.site.register(ProjectUser)
 admin.site.register(Organisation)
+admin.site.register(Presentation)
--- user_interface/views.py
+++ user_interface/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.views.generic import TemplateView
 from user_interface.models import ProjectUser
+from presentations.models import Presentation
 
 
 class IndexView(TemplateView):
@@ -9,8 +10,14 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
 
+        projectuser = ProjectUser.objects.get(name='tester')
+
         context.update({
-            'user': ProjectUser.objects.get(name='tester')
+            'user': projectuser,
+            'presentation': Presentation.objects.filter(
+                organisation=projectuser.organisation
+            ),
+
         })
 
         return context
