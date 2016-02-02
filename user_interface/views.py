8e4824cb22853a3942008731534c0160e7894706
Artem Ulko <ulkoart@gmail.com>
2016-02-02 17:56:14 +0300

    add local settings

--- user_interface/views.py
+++ user_interface/views.py
@@ -4,3 +4,12 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+
+        context.update({
+            'test': 123
+        })
+
+        return context
