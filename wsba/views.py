e621663df150a47cb1c0efc15346ee924f92fdd1
wad <suguby@gmail.com>
2016-02-10 23:46:24 +0300

    рефактор имени и месторасполажения базового класса для вьюх по организации

--- wsba/views.py
+++ wsba/views.py
@@ -1,22 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from django.http import Http404
 from django.views.generic import TemplateView
-
-from presentations.models import Organisation
-
-
-class WsbaTemplateView(TemplateView):
-
-    def get_context_data(self, **kwargs):
-        context = super(WsbaTemplateView, self).get_context_data(**kwargs)
-        org_slug = kwargs.get('organisation', '')
-        try:
-            organisation = Organisation.objects.get(slug=org_slug)
-        except Organisation.DoesNotExist:
-            raise Http404()
-        context.update(organisation=organisation)
-        return context
 
 
 class IndexView(TemplateView):
