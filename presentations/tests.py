e38505ee30e238024ff7a1715fee9e9613ad7576
Avis <aao.t@yandex.ru>
2016-02-28 16:04:11 +0300

    for pull

--- presentations/tests.py
+++ presentations/tests.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
-
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
-# Create your tests here.
+
+class QuestionViewTests(TestCase):
+    def test_index_view_with_no_questions(self):
+        """
+        If no questions exist, an appropriate message should be displayed.
+        """
+        response = self.client.get(reverse('slide_view', ))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
