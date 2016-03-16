83c2fcedc64299814d97c4f1c1ae348517fa21e9
mitri4 <mitri4@bk.ru>
2016-03-16 23:59:17 +0300

    Тест на создание вопроса

--- cms/questions/test.py
+++ cms/questions/test.py
@@ -14,18 +14,30 @@ class QuestionViewTestsCase(TestCase):
         self.organisation = Organisation.objects.create(name='test', slug='test')
 
     def test_list_view(self):
-        self.url = reverse('cms:questions-list', kwargs={'organisation': self.organisation.slug})
-        self.question = Question.objects.create(number=1, text='test', answers_type='single')
-        response = self.client.get(self.url)
+        url = reverse('cms:questions-list', kwargs={'organisation': self.organisation.slug})
+        Question.objects.create(number=1, text='test', answers_type='single')
+        response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
-        self.assertInHTML(needle='<span class="label label-info">single</span>', haystack=response.rendered_content)
         self.assertTemplateUsed(response, 'cms/questions/list.html')
+        self.assertInHTML(needle='<span class="label label-info">single</span>', haystack=response.rendered_content)
 
     def test_detail_view(self):
-        pass
+        question = Question.objects.create(number=2, text='test?')
+        url = reverse('cms:questions-detail', kwargs={'organisation': self.organisation.slug,
+                                                      'question': question.id})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'cms/questions/detail.html')
+        self.assertInHTML(needle='<div class="panel-body">test?</div>', haystack=response.rendered_content)
 
     def test_create_view(self):
-        pass
+        url = reverse('cms:questions-add', kwargs={'organisation': self.organisation.slug})
+        response_get = self.client.get(url)
+        response_post = self.client.post(url, {'number': 1, 'text': 'new_question', 'answers_type': 'multi'})
+        self.assertEquals(response_get.status_code, 200)
+        self.assertEquals(response_post.status_code, 302)
+        self.assertEqual(Question.objects.count(), 1)
+        self.assertTemplateUsed(response_get, 'cms/questions/edit.html')
 
     def test_update_view(self):
         pass
