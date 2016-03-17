02888ee9d5991b1605ea32b718fb4280a3092c35
Artem Ulko <ulkoart@gmail.com>
2016-03-17 22:18:49 +0300

    сделал навигацию по слайду

--- user_interface/urls.py
+++ user_interface/urls.py
@@ -7,5 +7,5 @@ from .views import OrganisationIndexView, ShowPresentation, GoPresentation
 urlpatterns = [
     url(r'^$', OrganisationIndexView.as_view(), name='organisation_detail'),
     url(r'^presentation/(?P<pk>\d+)/$', ShowPresentation.as_view(), name='show_presentation'),
-    url(r'^presentation/(?P<pk>\d+)/go/$', GoPresentation.as_view(), name='go_presentation'),
+    url(r'^presentation/(?P<presentation_id>\d+)/slides/(?P<slide_id>\d+)$', GoPresentation.as_view(), name='go_presentation'),
 ]
--- user_interface/views.py
+++ user_interface/views.py
@@ -46,12 +46,14 @@ class ShowPresentation(OrganisationTemplateView):
     def get_context_data(self, **kwargs):
         context = super(ShowPresentation, self).get_context_data(**kwargs)
         presentation_id = kwargs.get('pk', '')
+        start_slide = CoreSlide.objects.filter(presentation_id=presentation_id).order_by('position')[0]
         try:
             presentation = Presentation.objects.get(id=presentation_id)
         except Organisation.DoesNotExist:
             raise Http404()
         context.update(
-                presentation=presentation
+                presentation=presentation,
+                start_slide=start_slide,
         )
         return context
 
@@ -61,17 +63,31 @@ class GoPresentation(OrganisationTemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(GoPresentation, self).get_context_data(**kwargs)
-        presentation_id = kwargs.get('pk', '')
+        presentation_id = kwargs.get('presentation_id', '')
+        slide_id = kwargs.get('slide_id', '')
+
         try:
             presentation = Presentation.objects.get(id=presentation_id)
-            slides = CoreSlide.objects.filter(presentation=presentation)
-            slide=slides[0]
+            slide = CoreSlide.objects.get(id=slide_id)
         except Organisation.DoesNotExist:
             raise Http404()
+
+        try:
+            previous_url = CoreSlide.objects.filter(position__lt=slide.position).order_by('-position')[0].id
+        except IndexError:
+            previous_url = False
+
+        try:
+            next_url = CoreSlide.objects.filter(position__gt=slide.position).order_by('position')[0].id
+        except IndexError:
+            next_url = False
+
         context.update(
                 presentation=presentation,
                 slide=slide,
-                slides=slides,
+                len=len(CoreSlide.objects.filter(presentation=presentation_id)),
+                next_url=next_url,
+                previous_url=previous_url,
 
         )
         return context
