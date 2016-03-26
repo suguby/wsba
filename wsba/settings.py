5aab136246e4807638d938ea31e74184f3da3dde
wad <suguby@gmail.com>
2016-03-26 14:07:10 +0300

    багфикс MEDIA_URL

--- wsba/settings.py
+++ wsba/settings.py
@@ -140,5 +140,5 @@ STATICFILES_DIRS = (
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'extra', 'static')
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'extra', 'media')
