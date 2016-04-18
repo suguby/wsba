c250e4783e24322ce522e83b7161cc9c5c726bf0
ulkoart <ulkoart@gmail.com>
2016-04-18 14:36:20 +0300

    разбил тесты

--- user_interface/tests.py
+++ user_interface/tests.py
@@ -6,29 +6,39 @@ from presentations.models import Organisation, Presentation
 # Create your tests here.
 
 
-class ProjectUserTestCase(TestCase):
+class PresentationsListView(TestCase):
     def setUp(self):
         self.org = Organisation.objects.create(name='testorg', slug='testorg')
         self.user = ProjectUser.objects.create(name='tester1', organisation=self.org)
         self.url = reverse(viewname='organisation_detail', kwargs=dict(organisation=self.org.slug,))
-
-    def test_presentations(self):
-            presentation_1 = Presentation.objects.create(
+        self.presentation_1 = Presentation.objects.create(
                     organisation=self.org,
                     name='test_presentation1',
                     description='описание 1',
             )
-            presentation_2 = Presentation.objects.create(
+        self.presentation_2 = Presentation.objects.create(
                     organisation=self.org,
                     name='test_presentation2',
                     description='описание 2',
             )
-            response = self.client.get(self.url)
-            self.assertContains(response, status_code=200, text=self.org.slug)
-            self.assertContains(response, status_code=200, text=presentation_1.name)
-            self.assertContains(response, status_code=200, text=presentation_2.name)
-            self.assertContains(response, status_code=200, text='посмотреть', count=2)
-            self.assertInHTML(needle='<a href="/{}/presentation/{}/">посмотреть</a>'.format(self.org.slug, presentation_1.id),
-                              haystack=response.rendered_content)
-            self.assertInHTML(needle='<a href="/{}/presentation/{}/">посмотреть</a>'.format(self.org.slug, presentation_2.id),
-                              haystack=response.rendered_content)
+
+    def test_presentations(self):
+
+        response = self.client.get(self.url)
+        self.assertContains(response, status_code=200, text='tester')
+        self.assertContains(response, status_code=200, text=self.org.slug)
+        self.assertContains(response, status_code=200, text=self.presentation_1.name)
+        self.assertContains(response, status_code=200, text=self.presentation_2.name)
+
+
+
+class PresentationsButtons(PresentationsListView):
+
+    def test_presentations_detail(self):
+
+        response = self.client.get(self.url)
+        self.assertContains(response, status_code=200, text='посмотреть', count=2)
+        self.assertInHTML(needle='<a href="/{}/presentation/{}/">посмотреть</a>'.format(self.org.slug, self.presentation_1.id),
+                          haystack=response.rendered_content)
+        self.assertInHTML(needle='<a href="/{}/presentation/{}/">посмотреть</a>'.format(self.org.slug, self.presentation_2.id),
+                          haystack=response.rendered_content)
